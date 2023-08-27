abe8e18c1167a3df09eeda769df7a14842395b41
yatskovets <yatskovets.d@gmail.com>
2023-08-27 13:51:08 +0300

    basic layout

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <nav className='app-nav'>
+            <div className="item">
+                <a>Profile</a>
+            </div>
+            <div className="item">
+                <a>Messages</a>
+            </div>
+            <div className="item">
+                <a>News</a>
+            </div>
+            <div className="item">
+                <a>Music</a>
+            </div>
+            <div className="item">
+                <a>Settings</a>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+        <div className='app-content'>
+            <img src='https://t4.ftcdn.net/jpg/02/76/08/07/360_F_276080724_hltnCyDjcqAyRtLzDYo3T2jXbBtCD7fl.jpg' />
+            <div>
+                Ava + Description
+            </div>
+            <div>
+                My Posts
+                <div>
+                    New Posts
+                </div>
+                <div> 
+                    <div>post 1</div>
+                    <div>post 2</div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
